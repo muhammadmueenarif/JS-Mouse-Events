bbc428c22df91088746ff2dc0c1762248ec53c22
Mueen Arif <muhammadmueenarif786@gmail.com>
2024-09-25 09:16:41 +0500

    mouse events of  js

---
+++ script.js
@@ -0,0 +1,11 @@
+function Load() {
+    document.images["img1"].src= "yellow.png";
+}
+
+function MouseOver() {
+    document.images["img1"].src= "blackpink.png";
+}
+
+function MouseOut() {
+    document.images["img1"].src = "blackyellow.png";
+}
